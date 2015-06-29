afe3fb05e272029a88964b33d27594c83d2f84c5
Oleg Voloshyn <oleg.voloshyn@ex.ua>
2015-06-29 09:53:23 +0300

    fixed datetime formatting

--- framework/controllers/DocumentController.php
+++ framework/controllers/DocumentController.php
@@ -12,9 +12,6 @@ class DocumentController extends CController
     public function viewAction($lang, $cat_slug, $doc_slug) {
         //$locale = Locale::acceptFromHttp($_SERVER["HTTP_ACCEPT_LANGUAGE"]);
 
-        //echo $locale;
-        //die();
-
         $cats = $this->extra->getSql("SELECT c.id, c.slug, ct.name, ct.description FROM category c JOIN category_translate ct ON c.id = ct.category_id WHERE ct.lang = ? ORDER BY c.created_at",
             [$lang]);
 
@@ -40,14 +37,11 @@ class DocumentController extends CController
             'action' => 'error404'
         ]);
 
-        $doc['time'] = strtotime($doc['created_at']);
-        $formatter = new MessageFormatter($lang, "{0, date, long}");
-        $doc['formatted_time'] =  $formatter->format([$doc['time']]);
-        $doc['date'] =  date('Y-m-d', $doc['time']);
-
-        $this->view->setVar("cats", $cats);
-        $this->view->setVar("cat", $cat);
-        $this->view->setVar("doc", $doc);
+        $this->view->setVars([
+            'cats' => $cats,
+            'cat' => $cat,
+            'doc' => $doc
+        ]);
     }
 
 
